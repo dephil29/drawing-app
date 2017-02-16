ee2a80f4212c1bfeeb7223c51f7ea95f9a6774ec
David Phillips <dphillips@fvi.edu>
2017-02-15 21:46:31 -0500

    completed drawing app

---
+++ js/app.js
@@ -0,0 +1,70 @@
+var context = document.querySelector("canvas").getContext("2d");
+
+var selectedColor = $(".selected").css("background-color");
+
+function chooseColor(event) {
+    //Remove white ring from other colors
+    $(event.target).siblings().removeClass("selected");
+    //Make selected color have the white ring
+    $(event.target).addClass("selected");
+    //Set selected color to current color
+    selectedColor = $(event.target).css("background-color")
+}
+//When clicking on a color
+$(".controls li").click(chooseColor);
+
+
+//When "New Color" button is pressed
+$("#revealColorSelect").click(function() {
+    //hide/unhide the color select div
+    $("#colorSelect").toggle();
+});
+
+function changeColor() {
+    var color = "rgb(" + $("#red").val() + ", " + $("#green").val() + ", " + $("#blue").val() + ")";
+    $("#newColor").css("background-color", color);
+
+}
+
+//When color sliders changes
+$("input").on("input", function() {
+    changeColor();
+});
+
+$("#addNewColor").click(function() {
+    var newColor = $("<li></li>").css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append(newColor);
+    newColor.click(chooseColor);
+});
+//update the new color box
+
+//When "Add Color" button is pressed
+//append the new color to the controls ul
+//select the new color
+
+//on mouse events on the canvas
+//draw lines
+var lastMouseEvent;
+var isMouseDown = false;
+
+$("canvas").mousedown(function(event) {
+    lastMouseEvent = event;
+    isMouseDown = true;
+});
+
+$("canvas").mousemove(function(currentMouseEvent) {
+    if(isMouseDown === true) {
+        context.beginPath();
+        context.moveTo(lastMouseEvent.offsetX, lastMouseEvent.offsetY);
+        context.lineTo(currentMouseEvent.offsetX, currentMouseEvent.offsetY);
+        context.strokeStyle = selectedColor;
+        context.lineWidth = $("#thickness").val();
+        context.stroke();
+        console.log(lastMouseEvent.offsetX, currentMouseEvent.offsetX);
+        lastMouseEvent = currentMouseEvent;
+    }
+});
+
+$("canvas").mouseup(function() {
+    isMouseDown = false;
+});
